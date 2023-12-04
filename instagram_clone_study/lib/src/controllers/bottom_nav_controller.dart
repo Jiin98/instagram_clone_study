8864d87c8d5d8b78339742d1fdb210253d6488ef
jiin <moonjiin98@gmail.com>
2023-12-04 22:07:14 +0900

    feat: bottomHistory 버그 수정 및 showDialog 기능 구현

--- instagram_clone_study/lib/src/controllers/bottom_nav_controller.dart
+++ instagram_clone_study/lib/src/controllers/bottom_nav_controller.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:instagram_clone_study/src/components/message_popup.dart';
 import 'package:instagram_clone_study/src/pages/upload.dart';
 
 enum PageName { HOME, SEARCH, UPLOAD, ACTIVITY, MYPAGE }
@@ -26,12 +30,29 @@ class BottomNavController extends GetxController {
     pageIndex(value);
     if (!hasGesture) return;
     print('bottomHistory: $bottomHistory');
-    bottomHistory.add(value);
-    print('Added bottomHistory: $bottomHistory');
+    if (bottomHistory.last != value) {
+      bottomHistory.add(value);
+      print('Added bottomHistory: $bottomHistory');
+    }
+    // if (bottomHistory.contains(value)) {
+    //   bottomHistory.remove(value);
+    // }
+    // bottomHistory.add(value);
+    // print('Added bottomHistory: $bottomHistory');
   }
 
   Future<bool> willPopAction() async {
     if (bottomHistory.length == 1) {
+      showDialog(
+          context: Get.context!,
+          builder: (context) => MessagePopup(
+                title: '시스템',
+                message: '종료하시겠습니까?',
+                okCallback: () {
+                  exit(0);
+                },
+                cancleCallback: Get.back,
+              )); 
       print('Exit!');
       return true;
     } else {
